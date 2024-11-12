cc6069d08fb15258441b9787a4015c807b84d013
DanielBarret0 <josedanielbarreto@gmail.com>
2024-11-12 15:36:17 -0300

    feat: ensure transcation can be updated

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,14 +1,15 @@
-"use client"
+"use client";
 
-import { Transaction } from "@prisma/client"
-import { ColumnDef } from "@tanstack/react-table"
-import TransactionTypeBadge from "../_components/type-badge"
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import TransactionTypeBadge from "../_components/type-badge";
 import { Button } from "@/app/_components/ui/button";
-import { PencilIcon, TrashIcon } from "lucide-react";
-import { TRANSACTION_CATEGORY_LABELS, TRANSACTION_PAYMENT_METHOD_LABELS } from "@/app/_constants/transaction";
-
-
-
+import { TrashIcon } from "lucide-react";
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "@/app/_constants/transaction";
+import EditTransactionButton from "../_components/edit-transaction-button";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -18,20 +19,20 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: ({row: {original: transaction}}) => (
-      <TransactionTypeBadge transaction={transaction}/>
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypeBadge transaction={transaction} />
     ),
   },
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({row: {original: transaction}}) => 
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
-    header: "Método de pagamento",
-    cell: ({row: {original: transaction}}) => 
+    header: "Método de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
@@ -56,12 +57,10 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "actions",
     header: "Ações",
-    cell: () => {
+    cell: ({ row: { original: transaction } }) => {
       return (
         <div className="space-x-1">
-          <Button variant="ghost" size="icon" className="text-muted-foreground">
-            <PencilIcon />
-          </Button>
+          <EditTransactionButton transaction={transaction} />
           <Button variant="ghost" size="icon" className="text-muted-foreground">
             <TrashIcon />
           </Button>
@@ -69,4 +68,4 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
       );
     },
   },
-]
+];
