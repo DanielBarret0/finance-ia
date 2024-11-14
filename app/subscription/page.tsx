6cfaca66508727743659b48f4bebc4b08aae7434
DanielBarret0 <josedanielbarreto@gmail.com>
2024-11-14 16:32:31 -0300

    fix: upset transaction function

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,13 @@
-import Navbar from "../_components/ui/navbar"
+import { redirect } from "next/navigation";
+import Navbar from "../_components/ui/navbar";
+import { auth } from "@clerk/nextjs/server";
 
-const SubscriptionPage = () => {
-    return <Navbar/>
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return <Navbar />;
 };
 
 export default SubscriptionPage;
